a6654b9984088158e7e64ca334206b25326c7bac
PailletJuanPablo <pailletjuanpablo@gmail.com>
2018-12-31 02:22:25 -0300

    removed insecure routes

--- routes/artista.js
+++ routes/artista.js
@@ -1,23 +1,22 @@
-'use strict';
+"use strict";
 //Importamos express
-var express = require('express');
+var express = require("express");
 // Importamos modelo de artistas
-var ArtistController = require('../controllers/artista');
+var ArtistController = require("../controllers/artista");
 // Llamamos al metodo Router de Express para poder utilizar rutas
 var api = express.Router();
 //Importamos middleware de autenticacion
-var md_auth = require('../middlewares/authenticated');
+var md_auth = require("../middlewares/authenticated");
 //Importamos modulo para utilizar imagenes
-var multipart = require('connect-multiparty');
-var md_upload = multipart({uploadDir: './uploads/artists' });
+var multipart = require("connect-multiparty");
+var md_upload = multipart({ uploadDir: "./uploads/artists" });
 
-api.get('/artist/:id', md_auth.ensureAuth, ArtistController.getArtist);
-api.get('/artists/:page?', md_auth.ensureAuth, ArtistController.getArtists);
+api.get("/artist/:id", md_auth.ensureAuth, ArtistController.getArtist);
+api.get("/artists/:page?", md_auth.ensureAuth, ArtistController.getArtists);
 
-api.post('/artist', md_auth.ensureAuth, ArtistController.saveArtist);
-api.post('/artist/:id', md_auth.ensureAuth, ArtistController.updateArtist);
-api.post('/delete-artist/:id', md_auth.ensureAuth, ArtistController.deleteArtist);
-api.post('/upload-image-artist/:id', [md_auth.ensureAuth,md_upload], ArtistController.uploadImage);
-api.get('/get-image-artist/:imageFile', ArtistController.getImageFile);
+api.post("/artist", ArtistController.saveArtist);
+api.post("/artist/:id", md_auth.ensureAuth, ArtistController.updateArtist);
+api.post("/delete-artist/:id", md_auth.ensureAuth, ArtistController.deleteArtist);
+api.post("/upload-image-artist/:id", [md_auth.ensureAuth, md_upload], ArtistController.uploadImage);
+api.get("/get-image-artist/:imageFile", ArtistController.getImageFile);
 module.exports = api;
-
--- routes/song.js
+++ routes/song.js
@@ -1,23 +1,22 @@
-'use strict';
+"use strict";
 //Importamos express
-var express = require('express');
+var express = require("express");
 // Importamos modelo de artistas
-var SongController = require('../controllers/cancion');
+var SongController = require("../controllers/cancion");
 // Llamamos al metodo Router de Express para poder utilizar rutas
 var api = express.Router();
 //Importamos middleware de autenticacion
-var md_auth = require('../middlewares/authenticated');
+var md_auth = require("../middlewares/authenticated");
 //Importamos modulo para utilizar imagenes
-var multipart = require('connect-multiparty');
-var md_upload = multipart({uploadDir: './uploads/songs',autoFields :true });
+var multipart = require("connect-multiparty");
+var md_upload = multipart({ uploadDir: "./uploads/songs", autoFields: true });
 
-api.get('/song/:id', md_auth.ensureAuth, SongController.getSong);
-api.get('/songs/:page?', md_auth.ensureAuth, SongController.getSongs);
+api.get("/song/:id", md_auth.ensureAuth, SongController.getSong);
+api.get("/songs/:page?", md_auth.ensureAuth, SongController.getSongs);
 
-api.post('/song', md_auth.ensureAuth, SongController.saveSong);
-api.post('/song/:id', md_auth.ensureAuth, SongController.updateSong);
-api.post('/delete-song/:id', md_auth.ensureAuth, SongController.deleteSong);
-api.post('/upload-song/:id', [md_auth.ensureAuth,md_upload], SongController.uploadAudio);
-api.get('/get-audio/:audioFile', SongController.getAudioFile);
+api.post("/song", SongController.saveSong);
+api.post("/song/:id", md_auth.ensureAuth, SongController.updateSong);
+api.post("/delete-song/:id", md_auth.ensureAuth, SongController.deleteSong);
+api.post("/upload-song/:id", [md_auth.ensureAuth, md_upload], SongController.uploadAudio);
+api.get("/get-audio/:audioFile", SongController.getAudioFile);
 module.exports = api;
-
